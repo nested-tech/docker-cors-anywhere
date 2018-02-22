3f7ec2c6e3c1ab3f80c55827910f8b6b175b1220
Darren Oakley <daz.oakley@gmail.com>
2018-02-22 14:28:20 +0000

    Add the working code

---
+++ index.js
@@ -0,0 +1,10 @@
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+var port = process.env.PORT || 9000;
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+  originWhitelist: [],
+  removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+  console.log('Running CORS Anywhere on ' + host + ':' + port);
+})
